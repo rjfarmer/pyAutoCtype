6fa91678e933810cc6f802867a672c78cb095990
Robert Farmer <r.j.farmer@uva.nl>
2018-07-07 16:11:04 +0200

    Add basic test code

---
+++ test/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int intFunc(int i){
+    return 2*i;
+}
+
+int intFunc2(int i, int j){
+    return i+j;
+}
+
+float floatFunc(float i){
+    return 2.0*i;
+}
+
+float floatFunc2(float i, float j){
+    return i+j;
+}
---
+++ test/test.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int intFunc(int i);
+int intFunc2(int i, int j);
+float floatFunc(float i);
+float floatFunc2(float i, float j);
+
