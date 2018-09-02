3b68e93e5d03ae9572b5466d4f1aaa763923efcb
Robert Farmer <r.j.farmer@uva.nl>
2018-09-02 20:42:40 +0200

    Revert "More work on parsing dwarf data"
    This reverts commit 008d850d6ecb4024cf2d99bb23ffdef7b8b5a49e.

--- src/loadLib.py
+++ src/loadLib.py
@@ -1,13 +1,9 @@
 from __future__ import print_function
 import ctypes
-import collections
 from elftools.elf.elffile import ELFFile
 
 
-import utils
-
-
-filename = '../test/libtester.so'
+filename = 'test/libtester.so'
 
 def loadFile(filename):
     f=open(filename,'rb')
@@ -18,32 +14,19 @@ def loadFile(filename):
         raise ValueError("No debug symbols found")
     
     return elffile.get_dwarf_info()
-    
-def get_name_val(element):
-	try:
-		x = {
-			'type':element.attributes['DW_AT_type'].value,
-			'form':element.attributes['DW_AT_type'].form #bytesize?
-			}
-	except KeyError:
-		x = None
-	
-	return element.attributes['DW_AT_name'].value.decode(),x
+
+
+def get_meta(dwarf):
+    res={}
+    for CU in dwarf.iter_CUs():
+        top = CU.get_top_DIE() 
+        name = top['']
+
+
 
 x=loadFile(filename)
-#def get_meta(dwarf):
+get_meta(x)
 
-dwarf=x
-res={}
-for CU in dwarf.iter_CUs():
-	top = CU.get_top_DIE() 
-	fname = top.attributes['DW_AT_name'].value.decode()
-	folder = top.attributes['DW_AT_comp_dir'].value.decode()
-	language = int(top.attributes['DW_AT_language'].value)
-	res[fname] = {'folder':folder,'laguage':utils.get_language(language)} 
-	
-	if top.has_children:
-		res[fname]['comp'] = collections.OrderedDict()
-		for i in top.iter_children():
-			name, val = get_name_val(i)
-			res[fname]['comp'][name]=val
+        
+
+
